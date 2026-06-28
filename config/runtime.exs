3e0574494b5ee04d5df4170fe8636e80be2ca52b
3top1a <filip@rusz.space>
2026-06-28 17:04:02 +0200

    use a sane psql port

--- config/runtime.exs
+++ config/runtime.exs
@@ -20,7 +20,7 @@ if config_env() != :test do
       username: System.get_env("PGUSER", "postgres"),
       password: System.get_env("PGPASSWORD", "postgres"),
       hostname: System.get_env("PGHOST", "localhost"),
-      port: String.to_integer(System.get_env("PGPORT", "5433")),
+      port: String.to_integer(System.get_env("PGPORT", "5432")),
       database: System.get_env("PGDATABASE", "masaryk_ex_#{config_env()}")
   end
 
